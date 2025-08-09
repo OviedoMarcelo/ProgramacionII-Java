e5cfbf6fec41ce3aa0ce5dd7234f67202b2be868
Marcelo Oviedo <oviedommarcelo@gmail.com>
2025-08-08 23:00:36 -0300

    Se sube hasta el ejercicio 3

---
+++ TP 1/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Marcelo Oviedo
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); //Declaro una instancia de scanner
+        System.out.println("Hola Java");
+
+      
+    }
+}
---
+++ TP 1/programa3-imprimir/src/programa3/imprimir/Programa3Imprimir.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programa3.imprimir;
+
+/**
+ *
+ * @author Oviedo Marcelo
+ */
+public class Programa3Imprimir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        String nombre = "Marcelo";
+        int edad = 33;
+        double altura = 1.78;
+        boolean estudiante = true;
+        System.out.println("Nombre: "+nombre+"\nEdad: "+edad+"\nAltura: "+altura+"\nEstudiante: "+estudiante);
+    }
+    
+}
